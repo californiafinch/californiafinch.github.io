1c73dd55514244187e864dd54f6b61cee8b323d0
california_finch <130946789+californiafitzleo@users.noreply.github.com>
2026-07-27 15:32:45 +0800

    修复lazyload初始化时机：延迟到siteInit中执行，避免脚本加载顺序导致的错误

--- themes/shoka/source/js/_app/global.js
+++ themes/shoka/source/js/_app/global.js
@@ -20,17 +20,7 @@ var oWinHeight = window.innerHeight;
 var oWinWidth = window.innerWidth;
 var LOCAL_HASH = 0, LOCAL_URL = window.location.href;
 var pjax;
-var lazyload = lozad('img, [data-background-image]', {
-    loaded: function(el) {
-        el.addClass('lozaded');
-    },
-    error: function(el) {
-        if (el.tagName === 'IMG' && !el.dataset.err) {
-            el.dataset.err = '1';
-            el.src = CONFIG.root + 'assets/404.svg';
-        }
-    }
-})
+var lazyload;
 
 var Loader = {
   timer: null,
